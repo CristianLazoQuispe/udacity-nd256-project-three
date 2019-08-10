1952709342e31313bc6c18e95ceae84d9648a696
Stephen Lewis <code@stephenlewis.me>
2019-08-10 09:48:14 +0100

    Remove 0-9 constraint from problem 6 solution

--- problem_6.py
+++ problem_6.py
@@ -2,13 +2,9 @@ class EmptyList(Exception):
     pass
 
 
-class UdacityLies(Exception):
-    pass
-
-
 def get_min_max(ints):
     """
-    Return a tuple(min, max) from the given list of unsorted integers (0-9)
+    Return a tuple(min, max) from the given list of unsorted integers
 
     Args:
     ints(list): list of integers containing one or more integers
@@ -19,12 +15,9 @@ def get_min_max(ints):
     if len(ints) == 0:
         raise EmptyList
 
-    current_min = 9
-    current_max = 0
+    current_min = current_max = ints[0]
 
-    for number in ints:
-        if number < 0 or number > 9:
-            raise UdacityLies
+    for number in ints[1:]:
         if number < current_min:
             current_min = number
         if number > current_max:
@@ -39,19 +32,8 @@ def get_min_max(ints):
 assert get_min_max([0, 1]) == (0, 1)
 assert get_min_max([1, 0]) == (0, 1)
 assert get_min_max([5, 3, 2, 1, 9, 0, 7]) == (0, 9)
+assert get_min_max([-2, -1, 0, 1, 2]) == (-2, 2)
 assert get_min_max([5]) == (5, 5)
-
-try:
-    get_min_max([0, -1])
-    assert False, 'You promised the list would only contain the integers 0-9, Udacity'
-except UdacityLies:
-    assert True
-
-try:
-    get_min_max([0, 10])
-    assert False, 'You promised the list would only contain the integers 0-9, Udacity'
-except UdacityLies:
-    assert True
 
 try:
     get_min_max([])
