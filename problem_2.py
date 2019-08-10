48cd48a33963b813cdf491b51ce9ec322ea8cc99
Stephen Lewis <code@stephenlewis.me>
2019-08-10 12:36:00 +0100

    Refactor to use a loop, rather than recursion

--- problem_2.py
+++ problem_2.py
@@ -2,6 +2,14 @@ class EmptyHaystack(Exception):
     pass
 
 
+def is_sorted(stack):
+    return stack[0] < stack[-1] if len(stack) > 1 else True
+
+def sorted_stack_contains_needle(stack, needle):
+    if not stack:
+        return False
+    return stack[0] <= needle <= stack[-1]
+
 def rotated_array_search(haystack, needle):
     """
     Find the given needle in the given haystack
@@ -15,18 +23,9 @@ def rotated_array_search(haystack, needle):
     if not haystack:
         raise EmptyHaystack()
 
-    def is_sorted(stack):
-        return stack[0] < stack[-1] if len(stack) > 1 else True
+    start_index = 0
 
-    def sorted_stack_contains_needle(stack, needle):
-        if not stack:
-            return False
-        return stack[0] <= needle <= stack[-1]
-
-    def recurse(haystack, needle, start_index):
-        if not haystack:
-            return -1
-
+    while len(haystack) > 0:
         mid_index = len(haystack) // 2
         mid_value = haystack[mid_index]
 
@@ -38,11 +37,12 @@ def rotated_array_search(haystack, needle):
         right_stack = haystack[mid_index + 1:]
 
         if is_sorted(left_stack) and sorted_stack_contains_needle(left_stack, needle):
-            return recurse(left_stack, needle, start_index)
+            haystack = left_stack
         else:
-            return recurse(right_stack, needle, start_index + mid_index + 1)
+            haystack = right_stack
+            start_index = start_index + mid_index + 1
 
-    return recurse(haystack, needle, 0)
+    return -1
 
 
 # ----------------------------------------------------------------------------------------------------------------------
